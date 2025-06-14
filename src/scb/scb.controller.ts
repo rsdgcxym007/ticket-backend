6fc5469a31f3279854e227c88549f450844b02d9
rsdgcxym007 <590107030026@dpu.ac.th>
2025-06-14 18:39:58 +0700

    #fix

---
+++ src/scb/scb.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Post, Body } from '@nestjs/common';
+
+@Controller('api/scb')
+export class ScbController {
+  @Post('payment-webhook')
+  handlePayment(@Body() body: any) {
+    console.log('📥 [SCB Payment Webhook]', body);
+    return { status: 'RECEIVED' };
+  }
+}
