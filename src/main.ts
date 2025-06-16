19c97a0f823b7ba759f50c72c8bfa2eecfe80616
rsdgcxym007 <590107030026@dpu.ac.th>
2025-06-16 11:21:10 +0700

    #

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     prefix: '/uploads/slips',
   });
 
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
 }
 bootstrap();
